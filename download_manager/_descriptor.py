ba3c6fd11b415f579ec26e4697207d59e7548d04
deeenes <turei.denes@gmail.com>
2024-08-09 13:46:28 +0200

    `Descriptor`: process URL and fname also from kwargs

--- download_manager/_descriptor.py
+++ download_manager/_descriptor.py
@@ -1,4 +1,7 @@
+
+from typing import Any
 import os
+
 
 class Descriptor():
     """
@@ -9,12 +12,32 @@ class Descriptor():
         self._param = dict()
 
         url_fname, *_ = args + [None]
+        self._param.update(kwargs)
+        fname = url_fname or self.param('fname')
 
-        if os.path.exist(url_fname):
-            self.from_file(fname = url_fname)
+        if os.path.exist(fname):
+
+            self.from_file(fname = fname)
 
         else:
-            self._param["url"] = url_fname
+
+            self._param['url'] = url_fname
+
+        if not self.url:
+
+            raise ValueError('Missing URL')
+
 
     def from_file(self, fname: str):
         pass
+
+
+    def param(self, key: str) -> Any:
+
+        return self._param.get(key, None)
+
+
+    @property
+    def url(self) -> str:
+
+        return self.param('url')
