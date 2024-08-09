18e91c709a615c4029efb2768da2737f2e3c2bad
Balazs Bohar <bbohar@ic-5cg3423gzn>
2024-08-09 14:02:35 +0100

    Add CurlDownloader and RequestsDownloader classes

--- download_manager/_downloader.py
+++ download_manager/_downloader.py
@@ -3,9 +3,10 @@ import os
 import abc
 
 from . import _data
+from . import _descriptor
 
 __all__ = [
-    ,
+    "CurlDownloader", "RequestsDownloader"
 ]
 
 class AbstractDownloader(abc.ABC):
@@ -13,9 +14,28 @@ class AbstractDownloader(abc.ABC):
     Single download manager
     """
 
-    def __init__(self):
+    def __init__(self, desc: _descriptor.Descriptor):
         super().__init__()
+        self.desc = desc
 
     @abc.abstractmethod
     def download(self) -> None:
         raise NotImplementedError()
+
+
+class CurlDownloader():
+    """
+    Curl download
+    """
+
+    def __init__(self, desc: _descriptor.Descriptor):
+        super().__init__(desc)
+
+
+class RequestsDownloader():
+    """
+    Request download
+    """
+
+    def __init__(self, desc: _descriptor.Descriptor):
+        super().__init__(desc)
