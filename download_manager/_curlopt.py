078f02824a4d63cb06a8945f2a0e4b9791204e54
Nicolas Palacio <nicolaspalacio91@gmail.com>
2024-08-14 13:51:32 +0200

    Added `_curlopt.version_to_int` function
    Converts version number to int value for curl

--- download_manager/_curlopt.py
+++ download_manager/_curlopt.py
@@ -1,9 +1,9 @@
+import re
 
 import pycurl
 
 
-SYNONYMS =
-    {
+SYNONYMS = {
         'http_version': {
             'http_version_1_1': 1,
         }
@@ -19,3 +19,12 @@ def name_to_int(name: str) -> int | None:
             return value
 
     return None
+
+def version_to_int(ver: str):
+    ver = str(ver)
+    
+    if not re.match(r'^(?:curl_)?http_version', ver, re.IGNORECASE):
+        ver = ver.replace('.', '_')
+        ver = f'curl_http_version_{ver}'
+    
+    return name_to_int(ver)
