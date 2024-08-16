2c736ae396c67630367f5f5ed64bf71a866362b4
Nicolas Palacio <nicolaspalacio91@gmail.com>
2024-08-16 16:04:19 +0200

    Added tests for `RequestsDownloader`

---
+++ tests/test_requests.py
@@ -0,0 +1,44 @@
+import os
+
+import download_manager as dm
+
+
+def test_most_simple(http_url):
+
+    dl = dm.RequestsDownloader(dm.Descriptor(http_url))
+    dl.setup()
+    dl.download()
+
+    contents = dl.destination.read().decode('utf-8')
+
+    assert len(contents)
+    assert contents.startswith('<!DOCTYPE html')
+
+
+def test_simple_tls(https_url):
+
+    dl = dm.RequestsDownloader(dm.Descriptor(https_url))
+    dl.setup()
+    dl.download()
+
+    contents = dl.destination.read().decode('utf-8')
+
+    assert len(contents)
+    assert contents.startswith('<!DOCTYPE html')
+
+
+def test_simpl_to_file(http_url, download_dir):
+
+    path = os.path.join(download_dir, 'test.html')
+
+    dl = dm.RequestsDownloader(dm.Descriptor(http_url), path)
+    dl.setup()
+    dl.download()
+
+    assert os.path.exists(path)
+    assert dl.destination.closed
+
+    with open(path, 'r') as fp:
+        contents = fp.read()
+
+    assert contents.startswith('<!DOCTYPE html')
