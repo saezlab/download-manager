be8f55b7a8246a1eb91232de0929a42d7223a9ea
Nicolas Palacio <nicolaspalacio91@gmail.com>
2024-09-04 13:47:23 +0200

    Ordered class functions in `Descriptor` and fixed spacing

--- download_manager/_descriptor.py
+++ download_manager/_descriptor.py
@@ -21,6 +21,7 @@ class Descriptor(abc.Mapping):
     """
 
     def __init__(self, *args, **kwargs):
+
         self._param = dict()
 
         url_fname, *_ = list(args) + [None]
@@ -40,8 +41,11 @@ class Descriptor(abc.Mapping):
             raise ValueError('Missing URL')
 
         if not self['cainfo']:
+
             self['cainfo'] =  certifi.where()
+
         else:
+
             self['cainfo_override'] = self['cainfo']
 
         self['baseurl'] = self['url']
@@ -53,25 +57,42 @@ class Descriptor(abc.Mapping):
 
 
     def __iter__(self):
+
         return iter(self._param)
 
+
     def __contains__(self, value):
+
         return value in self._param.keys()
 
+
     def __len__(self):
+
         return len(self._param)
+
 
     def __getitem__(self, key: Any):
 
         return self._param.get(key, None)
 
+
     def __setitem__(self, key: Any, value: Any):
 
         self._param[key] = value
 
+
     def from_file(self, fname: str):
 
         self._param.update(_data._module_data(fname))
+
+
+    def get_headers_dict(self):
+
+        return dict(
+            elem.decode().split(': ', maxsplit=1)
+            for elem in self['headers']
+        )
+
 
     def set_get_post(self):
 
@@ -80,6 +101,7 @@ class Descriptor(abc.Mapping):
             self['qs'] = urllib.parse.urlencode(q)
 
         if self['json'] or self['multipart']:
+
             self['post'] = True
 
         self['url'] = (
@@ -87,21 +109,6 @@ class Descriptor(abc.Mapping):
                 if self['post'] or not self['qs'] else
             f'{self["baseurl"]}?{self["qs"]}'
         )
-
-
-    def set_multipart(self):
-
-        if self['multipart']:
-
-            multipart = {'data': {}, 'files': {}}
-
-            for k, v in self['multipart'].items():
-
-                v = str(v)
-                param_typ = 'files' if os.path.exists(v) else 'data'
-                multipart[param_typ][k] = v
-
-            self['multipart'] = multipart
 
 
     def set_headers(self):
@@ -128,9 +135,16 @@ class Descriptor(abc.Mapping):
             self['headers'].append(b'Content-Type: application/json')
 
 
-    def get_headers_dict(self):
+    def set_multipart(self):
 
-        return dict(
-            elem.decode().split(': ', maxsplit=1)
-            for elem in self['headers']
-        )
+        if self['multipart']:
+
+            multipart = {'data': {}, 'files': {}}
+
+            for k, v in self['multipart'].items():
+
+                v = str(v)
+                param_typ = 'files' if os.path.exists(v) else 'data'
+                multipart[param_typ][k] = v
+
+            self['multipart'] = multipart
