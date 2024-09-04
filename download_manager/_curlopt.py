8792b241d504105ce0e00ef01e1b234f246da98f
Nicolas Palacio <nicolaspalacio91@gmail.com>
2024-09-04 10:59:28 +0200

    Rearranged functions alphabetically and fixed spacing in `_curlopt`

--- download_manager/_curlopt.py
+++ download_manager/_curlopt.py
@@ -12,8 +12,38 @@ __all__ = [
     'process',
 ]
 
-
 SYNONYMS = {}
+
+
+def ensure_int(value: Any) -> int | None:
+
+    if isinstance(value, (int, bool)):
+
+        return int(value)
+
+    for n in (value, f'CURL_{value}'):
+
+        if (curl_int := getattr(pycurl, n.upper(), None)) is not None:
+
+            return curl_int
+
+    if isinstance(value, str):
+
+        return value.encode('utf-8')
+
+    return None
+
+
+def http_version(ver: str):
+
+    ver = str(ver)
+
+    if not re.match(r'^(?:curl_)?http_version', ver, re.IGNORECASE):
+
+        ver = ver.replace('.', '_')
+        ver = f'curl_http_version_{ver}'
+
+    return ver
 
 
 def process(key: str, value: Any) -> Any:
@@ -29,30 +59,3 @@ def process(key: str, value: Any) -> Any:
             value = proc(value)
 
     return ensure_int(value)
-
-
-def ensure_int(value: Any) -> int | None:
-
-    if isinstance(value, (int, bool)):
-
-        return int(value)
-
-    for n in (value, f'CURL_{value}'):
-        if (curl_int := getattr(pycurl, n.upper(), None)) is not None:
-            return curl_int
-
-    if isinstance(value, str):
-        return value.encode('utf-8')
-
-    return None
-
-
-def http_version(ver: str):
-
-    ver = str(ver)
-
-    if not re.match(r'^(?:curl_)?http_version', ver, re.IGNORECASE):
-        ver = ver.replace('.', '_')
-        ver = f'curl_http_version_{ver}'
-
-    return ver
