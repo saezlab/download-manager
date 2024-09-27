babda4ca647c659050d3950c1a972b9df3d055e9
deeenes <turei.denes@gmail.com>
2024-09-27 13:32:23 +0200

    test for parsing filename from Cont-Disp header

--- tests/test_downloader.py
+++ tests/test_downloader.py
@@ -10,3 +10,15 @@ def test_filename(http_url):
     assert d[2].filename == 'robots.txt'
 
 
+def test_filename_contdispos(http_url):
+
+    url = (
+        f'{http_url}/response-headers?'
+        'Content-Type=text/plain;%20charset=UTF-8&'
+        'Content-Disposition=attachment;%20filename%3d%22test.json%22'
+    )
+
+    man = dm.DownloadManager()
+    d = man._download(url, dest = False)
+
+    assert d[2].filename == 'test.json'
