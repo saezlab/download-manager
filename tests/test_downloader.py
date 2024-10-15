ef24b4222793d4812b6d055048b79e66b5603f0c
deeenes <turei.denes@gmail.com>
2024-10-15 13:53:19 +0200

    test for storing file name from cont disp header

--- tests/test_downloader.py
+++ tests/test_downloader.py
@@ -22,3 +22,17 @@ def test_filename_contdispos(http_url):
     d = man._download(url, dest = False)
 
     assert d[2].filename == 'test.json'
+
+
+def test_filename_contdispos(http_url):
+
+    url = (
+        f'{http_url}/response-headers?'
+        'Content-Type=text/plain;%20charset=UTF-8&'
+        'Content-Disposition=attachment;%20filename%3d%22test.json%22'
+    )
+
+    man = dm.DownloadManager()
+    d = man._download(url)
+
+    assert d[2].filename == 'test.json'
